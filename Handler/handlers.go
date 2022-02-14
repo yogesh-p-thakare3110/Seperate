1354c6f20dd7431d3c9a10c694cc7699127483a6
yogesh-p-thakare3110 <95970605+yogesh-p-thakare3110@users.noreply.github.com>
2022-02-14 18:48:25 +0530

    Add files via upload

---
+++ Handler/handlers.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/yogesh-p-thakare3110/Seperate/movie"
+)
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movie.Movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movie.Movies {
+		if item.ID == params["id"] {
+			movie.Movies = append(movie.Movies[:index], movie.Movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movie.Movies)
+}
+
+func GetMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movie.Movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content_Type", "application/json")
+	var movie movie.Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000))
+	movie.Movies = append(movie.Movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movie.Movies {
+		if item.ID == params["id"] {
+			movie.Movies = append(movie.Movies[:index], movie.Movies[index+1:]...)
+			var movie movie.Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movie.Movies = append(movie.Movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
